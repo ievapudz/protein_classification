4988130540ed35756bd6bad0594ac4e32ec019f2
Ieva Pudžiuvelytė <pudziuvelyteieva@gmail.com>
2020-10-03 19:03:49 +0300

    Add constants.hpp file

---
+++ src/constants.hpp
@@ -0,0 +1,17 @@
+#ifndef _CONSTANTS_
+#define _CONSTANTS_
+
+class Constants{
+private:
+    const static double gap_open_penalty_ = 4.0;
+    const static double gap_ext_penalty_ = 1.0;
+public:
+    static double gapOpenPenalty(){
+        return gap_open_penalty_;
+    }
+    static double gapExtPenalty(){
+        return gap_ext_penalty_;
+    }
+};
+
+#endif
