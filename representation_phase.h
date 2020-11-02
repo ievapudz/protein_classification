225dee85e40aa07800024843ad9e4343f6fc9ed0
Pudžiuvelytė Ieva <ieva.pudziuvelyte@mif.stud.vu.lt>
2020-11-02 20:39:25 +0200

    Upload RepresentationPhase

---
+++ representation_phase.h
@@ -0,0 +1,19 @@
+#ifndef _REPRESENTATION_PHASE_
+#define _REPRESENTATION_PHASE_
+#include "calculation_phase.h"
+#include "./src/biological_structures.h"
+#include "./src/file_workflow.h"
+
+class RepresentationPhase{
+        CalculationPhase* calculations_;
+        Protein& p_protein_;
+        Protein& q_protein_;
+    public:
+        RepresentationPhase(CalculationPhase* calculations);
+    void representAlignment();
+    void representNumeralAlignment();
+    void representIdentityScore();
+};
+
+
+#endif
