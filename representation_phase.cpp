407621aeaa32f4f501d2ec0ce8fbf5cd79243958
Ieva Pudžiuvelytė <pudziuvelyteieva@gmail.com>
2020-11-04 06:29:58 +0200

    Updated representation_phase.cpp

--- representation_phase.cpp
+++ representation_phase.cpp
@@ -13,7 +13,10 @@ void RepresentationPhase::representAlignment(){
 }
 
 void RepresentationPhase::representNumeralAlignment(){
-    
+    std::string alignment_file_name = std::to_string(calculations_->getSubstructureLength());
+    alignment_file_name.append("_"+p_protein_.getName()+"_"+q_protein_.getName()+"_numeral_alignment_file.txt");
+    TXTFile alignment_file(alignment_file_name);
+    alignment_file.writeData("./alignment_results_numeral/", calculations_->getAlignment().first, calculations_->getAlignment().second);
 }
 
 void RepresentationPhase::representIdentityScore(){
